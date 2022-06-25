c58e60feb5214555a3f46e056f2d96b5a82f3521
Waeyword <walldoyeamafaye@gmail.com>
2022-06-25 01:48:03 +0100

    minor change

--- I4G011493WMJ/blog/models.py
+++ I4G011493WMJ/blog/models.py
@@ -11,9 +11,9 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE) 
-    created_on = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now=True)
     text = models.TextField()
-    updated_on = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(auto_now=True)
 
 class Meta:
     ordering = ('-created_on')
